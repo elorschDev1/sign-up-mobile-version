1b68955e5410972753f1e9de053afbcb40523b10
edgarlorsch10 <elorschdev@gmail.com>
2025-10-24 17:17:49 +0300

    Today, I have covered the basics of designing a sign up form in react native, I have looked at concepts such as KeyBoardAvoidingView, which assist users to see what they are typing in, how different the onChangeText prop is from the onChange prop in react, and many others, in the following session, I will deal with client side validation and sending basic fetch requests from a react native frontend, I will try to see the similarities and differences between this kind of request and one done from a vanilla js or react frontend.

--- App.js
+++ App.js
@@ -1,11 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from "react";
+import { StyleSheet, Text, View,TextInput,Pressable,KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function App() {
+  let [username,setUserName]=useState("");
+  let [userNameError,setUserNameError]=useState("");
+  let [email,setEmail]=useState("");
+  let [emailError,setEmailError]=useState("");
+  let [phoneNumber,setPhoneNumber]=useState("");
+  let [phoneNumberError,setPhoneNumberError]=useState("");
+  let [password,setPassword]=useState("");
+  let [passwordError,setPasswordError]=useState("");
+  let [website,setWebsite]=useState("");
+  let [websiteError,setWebsiteError]=useState("");
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+     <View style={styles.field}> 
+      <KeyboardAvoidingView behavior={Platform.OS==="ios"?"padding":"height"}>
+          <Text style={styles.label}>Name:</Text>
+      <TextInput style={styles.input} value={username} onChangeText={(username)=>{
+        setUserName(username);
+        if(userNameError!=="")setUserNameError("");
+      }}
+      autoCorrect={false}
+      autoCapitilize="none"
+      
+      />
+      <Text style={styles.errorField}>{userNameError}</Text>
+      </KeyboardAvoidingView>
+     
+     </View>
+     <View style={styles.field}>
+    <KeyboardAvoidingView behavior={Platform.OS==="ios"?"padding":"height"}>
+         <Text  style={styles.label}>Email:</Text>
+      <TextInput style={styles.input} value={email} onChangeText={(email)=>{
+        if(emailError!=="")setEmailError("");
+        setEmail(email);
+      }}
+      keyboardType="email-address"
+      />
+      <Text style={styles.errorField}>{emailError}</Text>
+    </KeyboardAvoidingView>
+     </View>
+      <View style={styles.field}>
+    <KeyboardAvoidingView behavior={Platform.OS==="ios"?"padding":"height"}>
+        <Text  style={styles.label}>Phone Number:</Text>
+      <TextInput style={styles.input} value={phoneNumber} onChangeText={(phoneNumber)=>{
+        setPhoneNumber(phoneNumber);
+        if(phoneNumberError!=="")setPhoneNumberError("");
+      }}
+      keyboardType="phone-pad"
+      
+      />
+      <Text style={styles.errorField}>{phoneNumberError}</Text>
+    </KeyboardAvoidingView>
+      </View>
+     <View style={styles.field}>
+     <KeyboardAvoidingView behavior={Platform.OS==="ios"?"padding":"height"}>
+         <Text  style={styles.label}>Password:</Text>
+      <TextInput style={styles.input}
+      value={password}
+      onChangeText={(password)=>{
+        setPassword(password);
+        if(passwordError!=="")setPasswordError("");
+      }}
+      secureTextEntry={true}
+      />
+      <Text style={styles.errorField}>{passwordError}</Text>
+     </KeyboardAvoidingView>
+     </View>
+      <View style={styles.field}>
+      <KeyboardAvoidingView behavior={Platform.OS==="ios"?"padding":"height"}>
+           <Text  style={styles.label}>Website:</Text>
+      <TextInput style={styles.input}  value={website}  onChangeText={(website)=>{
+        setWebsite(website);
+        if(websiteError!=="")setWebsiteError("");
+      }}
+      keyboardType="url"
+      />
+      <Text style={styles.errorField}>{websiteError}</Text> 
+      </KeyboardAvoidingView>
+    </View>
+    <View style={styles.field}>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </Pressable>
+    </View>
     </View>
   );
 }
@@ -13,8 +94,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  
     alignItems: 'center',
     justifyContent: 'center',
+    padding:5
   },
+  field:{
+    marginTop:7
+  },
+  label:{
+    marginBottom:8,
+    fontSize:16
+  },
+  input:{
+    height:40,
+    marginBottom:16,
+    borderWidth:1,
+    borderColor:"#ccc",
+    borderRadius:8,
+    fontSize:16,
+    padding:9,
+    width:200,
+    
+  },
+  errorField:{
+    marginTop:8,
+    fontSize:16,
+    color:"red"
+  },
+  button:{
+    paddingVertical:8,
+    paddingHorizontal:16,
+    borderRadius:32,
+    minWidth:100,
+    alignItems:"center",
+    backgroundColor:"#111"
+  },
+  buttonText:{
+    color:"#fff",
+    fontSize:16
+  }
 });
